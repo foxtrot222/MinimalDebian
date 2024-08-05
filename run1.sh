6176501b457e697b6f01003f6fca7077c1d01ad1
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-05 13:47:50 +0530

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade
-apt install curl xorg pcmanfm i3 build-essential libpam0g-dev libxcb-xkb-dev
+apt install curl xorg i3 build-essential libpam0g-dev libxcb-xkb-dev
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
