b3a896c1d716b7ee45bd5cd2ea77ec0aaaa9b839
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-05 15:40:11 +0530

    Update setconf.sh

--- confs/setconf.sh
+++ confs/setconf.sh
@@ -2,6 +2,12 @@ sudo git clone https://github.com/dracula/rofi
 mkdir ~/.config/rofi 
 sudo cp rofi/theme/config1.rasi ~/.config/rofi/config.rasi
 
+mkdir mkdir ~/.config/i3status
+sudo cp i3status.conf ~/.config/i3status/i3status.conf
+
+mkdir ~/.config/i3
+sudo cp config ~/.config/i3/
+
 cp debian.png ~/Pictures/
 
 
