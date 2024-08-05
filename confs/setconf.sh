826614db46976516a8df79fe2490e65729950902
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-05 17:22:13 +0530

    Update setconf.sh

--- confs/setconf.sh
+++ confs/setconf.sh
@@ -2,6 +2,7 @@ sudo cp config.rasi ~/.config/rofi/config.rasi
 
 mkdir mkdir ~/.config/i3status
 sudo cp i3status.conf ~/.config/i3status/i3status.conf
+sudo chown $USER:$USER ~/.config/i3status/i3status.conf
 
 mkdir ~/.config/i3
 sudo cp config ~/.config/i3/
