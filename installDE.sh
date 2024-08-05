190102dca46043419d8d441830a89c3b35d3dff5
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-05 12:47:02 +0530

    Update installDE.sh

--- installDE.sh
+++ installDE.sh
@@ -1,10 +1,13 @@
 apt update
 apt upgrade
-apt install curl xorg pcmanfm i3 slim
-
-echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/Debian_12/ /' | tee /etc/apt/sources.list.d/home:manuelschneid3r.list
-curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/Debian_12/Release.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
-apt update
-apt install albert
+apt install curl xorg pcmanfm i3 build-essential libpam0g-dev libxcb-xkb-dev
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install zig
+git clone https://github.com/fairyglade/ly
+cd ly
+zig build
+zig build run
+zig build installsystemd
+systemctl enable ly.service
 
 echo "Installation complete!"
