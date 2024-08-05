ca60e4c43804b8bdec62afa19ee2ae3eaf782e0b
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-05 10:12:34 +0530

    Update installapps.sh

--- installapps.sh
+++ installapps.sh
@@ -2,20 +2,7 @@
 
 apt update
 apt upgrade
-apt install curl xorg polybar pavucontrol brightnessctl network-manager pcmanfm i3 connman build-essential libpam0g-dev libxcb-xkb-dev 
-
-install -d -m 0755 /etc/apt/keyrings
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-
-echo '
-Package: *
-Pin: origin packages.mozilla.org
-Pin-Priority: 1000
-' | sudo tee /etc/apt/preferences.d/mozilla 
-
-sudo apt-get update && sudo apt-get install firefox
+apt install curl xorg polybar pcmanfm i3 build-essential libpam0g-dev libxcb-xkb-dev 
 
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/Debian_12/ /' | tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/Debian_12/Release.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
