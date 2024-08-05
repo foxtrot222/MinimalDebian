5ad161d426d8a7ca793b71e137ac35c9d896381b
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-05 13:33:00 +0530

    Update installDE.sh

--- installDE.sh
+++ installDE.sh
@@ -1,10 +1,14 @@
-apt update
-apt upgrade
+sudo apt update
+sudo apt upgrade
 apt install curl xorg pcmanfm i3 build-essential libpam0g-dev libxcb-xkb-dev
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
 brew install zig
-git clone https://github.com/fairyglade/ly
-cd ly
+reboot
+sudo git clone https://github.com/fairyglade/ly
+sudo cd ly
 zig build
 zig build run
 zig build installsystemd
