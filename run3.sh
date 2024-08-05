e0c18bdb4f0b7047be3ccc48a5da73e55f1e1019
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-05 14:57:26 +0530

    Update run3.sh

--- run3.sh
+++ run3.sh
@@ -1,4 +1,4 @@
 brew uninstall zig
 sudo apt update
-sudo apt install pcmanfm network-manager brightnessctl pavucontrol blueman scrot feh
+sudo apt install pcmanfm network-manager brightnessctl pavucontrol blueman scrot feh lxappearance
 sudo apt autoremove
