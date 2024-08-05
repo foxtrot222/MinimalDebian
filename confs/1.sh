b6580b8535d773ee4d97b2588d0cb937378acd27
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-05 18:18:02 +0530

    Delete confs/1.sh

--- confs/1.sh
+++
@@ -1,6 +0,0 @@
-mkdir ~/.config/i3status
-sudo cp i3status.conf ~/.config/i3status/i3status.conf
-sudo chown $USER:$USER ~/.config/i3status/i3status.conf
-
-mkdir ~/.config/i3
-sudo cp config ~/.config/i3/
