478ab9e7ef3b10ed3a6ac2c4b4c707005ad5827a
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-06 12:36:42 +0530

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade
-apt install curl xorg i3 build-essential libpam0g-dev libxcb-xkb-dev pcmanfm network-manager brightnessctl pavucontrol blueman scrot feh lxappearance xfce4-terminal
+apt install curl xorg i3 build-essential libpam0g-dev libxcb-xkb-dev pcmanfm network-manager brightnessctl pavucontrol blueman scrot feh lxappearance xfce4-terminal rofi
 sudo apt remove xterm
 sudo apt autoremove
 echo "Apt packages installed, REBOOT THE SYSTEM and run run2.sh without root."
