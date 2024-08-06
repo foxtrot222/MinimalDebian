11af4e32fca4afe364195c66b0f48b83fa94cf33
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-06 11:41:52 +0530

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -2,11 +2,10 @@ sudo apt update
 sudo apt upgrade
 apt install curl xorg i3 build-essential libpam0g-dev libxcb-xkb-dev
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install zig
 echo "REBOOTING"
-sleep 5
+sleep 60
 /sbin/reboot
 
