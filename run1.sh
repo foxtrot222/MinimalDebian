b5ab0d0f4a42d534b8775543073713953f4d99f0
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-06 11:47:22 +0530

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,11 +1,6 @@
 sudo apt update
 sudo apt upgrade
-apt install curl xorg i3 build-essential libpam0g-dev libxcb-xkb-dev
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew install zig
-echo "REBOOTING"
-sleep 60
-/sbin/reboot
-
+apt install curl xorg i3 build-essential libpam0g-dev libxcb-xkb-dev pcmanfm network-manager brightnessctl pavucontrol blueman scrot feh lxappearance xfce4-terminal
+sudo apt remove xterm
+sudo apt autoremove
+echo "Apt packages installed"
