594d52191895038de3efb5a7f546d99f03670fe9
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-06 11:48:20 +0530

    Delete run3.sh

--- run3.sh
+++
@@ -1,5 +0,0 @@
-brew uninstall zig
-sudo apt update
-sudo apt install pcmanfm network-manager brightnessctl pavucontrol blueman scrot feh lxappearance xfce4-terminal
-sudo apt remove xterm
-sudo apt autoremove
