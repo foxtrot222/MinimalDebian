db97554141febba78eb9cd0fe38d681d6e51dc82
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2024-08-09 16:50:55 +0000

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install curl xorg i3 build-essential libpam0g-dev libxcb-xkb-dev pcmanfm network-manager brightnessctl pavucontrol blueman scrot feh lxappearance xfce4-terminal rofi xbindkeys imagemagick gthumb vlc htop vim neofetch alsa-utils xarchiver flatpak
+sudo apt install curl xorg i3 build-essential libpam0g-dev libxcb-xkb-dev pcmanfm network-manager brightnessctl pavucontrol blueman scrot feh lxappearance xfce4-terminal rofi xbindkeys imagemagick gthumb vlc htop vim neofetch xarchiver network-manager-gnome
 sudo apt autoremove
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 echo "Apt packages installed and flatpak added, REBOOT THE SYSTEM and run run2.sh without root."
